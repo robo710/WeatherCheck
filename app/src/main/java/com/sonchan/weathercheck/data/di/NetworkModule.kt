7ed2ce6dc6c0ce765c216b76efd35ffdfecf7c45
손찬형 <sonchanhyeong710@gmail.com>
2025-04-24 09:10:00 +0900

    feature: provideRetrofit 설정

--- app/src/main/java/com/sonchan/weathercheck/data/di/NetworkModule.kt
+++ app/src/main/java/com/sonchan/weathercheck/data/di/NetworkModule.kt
@@ -1,4 +1,22 @@
 package com.sonchan.weathercheck.data.di
 
-class NetworkModule {
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object NetworkModule {
+    @Provides
+    @Singleton
+    fun provideRetrofit(): Retrofit {
+        return Retrofit.Builder()
+            .baseUrl("https://apis.data.go.kr/1360000/VilageFcstInfoService_2.0/")
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+    }
 }
