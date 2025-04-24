466c3fa435ae3ab40bf6009d355b275c0985ea77
손찬형 <sonchanhyeong710@gmail.com>
2025-04-24 15:56:19 +0900

    feat: WeatherRepository 구현

--- app/src/main/java/com/sonchan/weathercheck/data/repository/WeatherRepository.kt
+++ app/src/main/java/com/sonchan/weathercheck/data/repository/WeatherRepository.kt
@@ -1,4 +1,31 @@
 package com.sonchan.weathercheck.data.repository
 
-class WeatherRepository {
+import com.sonchan.weathercheck.data.remote.api.WeatherApi
+import com.sonchan.weathercheck.data.remote.model.WeatherResponse
+import javax.inject.Inject
+
+class WeatherRepository @Inject constructor(
+    private val apiService: WeatherApi
+){
+    suspend fun getWeatherForecast(
+        serviceKey: String,
+        dataType: String,
+        numOfRows: Int = 1000,
+        pageNo: Int = 1,
+        baseDate: String,
+        baseTime: String,
+        nx: Int,
+        ny: Int
+    ): WeatherResponse {
+        return apiService.getWeatherForecast(
+            serviceKey = serviceKey,
+            dataType = dataType,
+            numOfRows = numOfRows,
+            pageNo = pageNo,
+            baseDate = baseDate,
+            baseTime = baseTime,
+            nx = nx,
+            ny = ny
+        )
+    }
 }
