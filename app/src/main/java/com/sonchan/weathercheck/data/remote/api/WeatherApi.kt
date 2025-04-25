91e3ffa3de6116bf3d5e4c0467d0944e2528a9e2
손찬형 <sonchanhyeong710@gmail.com>
2025-04-25 12:14:52 +0900

    feat: WEATHER_API_KEY 설정

--- app/src/main/java/com/sonchan/weathercheck/data/remote/api/WeatherApi.kt
+++ app/src/main/java/com/sonchan/weathercheck/data/remote/api/WeatherApi.kt
@@ -1,5 +1,6 @@
 package com.sonchan.weathercheck.data.remote.api
 
+import com.sonchan.weathercheck.BuildConfig
 import com.sonchan.weathercheck.data.remote.model.WeatherResponse
 import retrofit2.http.GET
 import retrofit2.http.Query
@@ -7,7 +8,7 @@ import retrofit2.http.Query
 interface WeatherApi {
     @GET("getVilageFcst")
     suspend fun getWeatherForecast(
-        @Query("serviceKey") serviceKey: String,
+        @Query("serviceKey") serviceKey: String = BuildConfig.WEATHER_API_KEY,
         @Query("dataType") dataType: String = "JSON",
         @Query("numOfRows") numOfRows: Int,
         @Query("pageNo") pageNo: Int,
--- app/src/main/java/com/sonchan/weathercheck/data/repository/WeatherRepositoryImpl.kt
+++ app/src/main/java/com/sonchan/weathercheck/data/repository/WeatherRepositoryImpl.kt
@@ -2,30 +2,21 @@ package com.sonchan.weathercheck.data.repository
 
 import com.sonchan.weathercheck.data.remote.api.WeatherApi
 import com.sonchan.weathercheck.data.remote.model.WeatherResponse
+import com.sonchan.weathercheck.domain.model.WeatherInfo
+import com.sonchan.weathercheck.domain.repository.WeatherRepository
 import javax.inject.Inject
 
 class WeatherRepositoryImpl @Inject constructor(
     private val apiService: WeatherApi
-){
-    suspend fun getWeatherForecast(
-        serviceKey: String,
-        dataType: String,
-        numOfRows: Int = 1000,
-        pageNo: Int = 1,
-        baseDate: Int,
-        baseTime: Int,
-        nx: Int,
-        ny: Int
-    ): WeatherResponse {
-        return apiService.getWeatherForecast(
-            serviceKey = serviceKey,
-            dataType = dataType,
-            numOfRows = numOfRows,
-            pageNo = pageNo,
-            baseDate = baseDate,
-            baseTime = baseTime,
-            nx = nx,
-            ny = ny
+): WeatherRepository{
+    override suspend fun getTodayWeatherInfo(): WeatherInfo {
+        val response = apiService.getWeatherForecast(
+            numOfRows = TODO(),
+            pageNo = TODO(),
+            baseDate = TODO(),
+            baseTime = TODO(),
+            nx = TODO(),
+            ny = TODO()
         )
     }
 }
