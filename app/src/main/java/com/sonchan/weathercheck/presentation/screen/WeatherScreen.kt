ac401377c77c1e65b11851ca06011fe8730ad180
손찬형 <sonchanhyeong710@gmail.com>
2025-04-26 15:21:25 +0900

    chore: Preview 설정

--- app/src/main/java/com/sonchan/weathercheck/presentation/screen/WeatherScreen.kt
+++ app/src/main/java/com/sonchan/weathercheck/presentation/screen/WeatherScreen.kt
@@ -2,10 +2,20 @@ package com.sonchan.weathercheck.presentation.screen
 
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import com.sonchan.weathercheck.presentation.component.preview.DevicePreviews
+import com.sonchan.weathercheck.ui.theme.WeatherCheckTheme
 
 @Composable
 fun WeatherScreen(
     modifier: Modifier = Modifier
 ){
 
+}
+
+@DevicePreviews
+@Composable
+fun WeatherScreenPreview(){
+    WeatherCheckTheme {
+        WeatherScreen()
+    }
 }
