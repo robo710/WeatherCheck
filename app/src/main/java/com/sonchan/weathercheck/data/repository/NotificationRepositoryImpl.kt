9b6b79e53dd738d802b8c3c1a4cae41898da3c64
손찬형 <sonchanhyeong710@gmail.com>
2025-04-28 18:22:24 +0900

    feat: NotificationRepositoryImpl 구현

--- app/src/main/java/com/sonchan/weathercheck/data/repository/NotificationRepositoryImpl.kt
+++ app/src/main/java/com/sonchan/weathercheck/data/repository/NotificationRepositoryImpl.kt
@@ -1,4 +1,38 @@
 package com.sonchan.weathercheck.data.repository
 
-class NotificationRepositoryImpl {
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.content.Context
+import android.os.Build
+import androidx.core.app.NotificationCompat
+import androidx.core.app.NotificationManagerCompat
+import com.sonchan.weathercheck.R
+import com.sonchan.weathercheck.domain.repository.NotificationRepository
+import javax.inject.Inject
+
+class NotificationRepositoryImpl @Inject constructor(
+
+): NotificationRepository{
+    override fun showNotification(context: Context) {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            val channelId = "your_channel_id"
+            val channelName = "Your Channel Name"
+            val importance = NotificationManager.IMPORTANCE_DEFAULT
+            val channel = NotificationChannel(channelId, channelName, importance).apply {
+                description = "Your Channel Description"
+            }
+            val notificationManager: NotificationManager = context.getSystemService(NotificationManager::class.java)
+            notificationManager.createNotificationChannel(channel)
+        }
+
+        val builder = NotificationCompat.Builder(context, "your_channel_id")
+            .setSmallIcon(R.drawable.ic_launcher_foreground)
+            .setContentTitle("Your Notification Title")
+            .setContentText("Your Notification Content")
+            .setPriority(NotificationCompat.PRIORITY_HIGH)
+
+        with(NotificationManagerCompat.from(context)) {
+            notify(1, builder.build())
+        }
+    }
 }
