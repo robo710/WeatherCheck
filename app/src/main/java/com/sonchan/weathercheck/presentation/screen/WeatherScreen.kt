bd977226b2e308aff8562b3afb0e838d7a299a51
손찬형 <sonchanhyeong710@gmail.com>
2025-04-28 09:20:28 +0900

    chore: 메인 ui 생성

--- app/src/main/java/com/sonchan/weathercheck/presentation/screen/WeatherScreen.kt
+++ app/src/main/java/com/sonchan/weathercheck/presentation/screen/WeatherScreen.kt
@@ -1,7 +1,12 @@
 package com.sonchan.weathercheck.presentation.screen
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import com.sonchan.weathercheck.presentation.component.preview.DarkThemeDevicePreviews
 import com.sonchan.weathercheck.presentation.component.preview.DevicePreviews
 import com.sonchan.weathercheck.ui.theme.WeatherCheckTheme
 
@@ -9,10 +14,16 @@ import com.sonchan.weathercheck.ui.theme.WeatherCheckTheme
 fun WeatherScreen(
     modifier: Modifier = Modifier
 ){
-
+    Column(
+        modifier
+            .fillMaxSize()
+            .background(MaterialTheme.colorScheme.background)
+    ) {
+    }
 }
 
 @DevicePreviews
+@DarkThemeDevicePreviews
 @Composable
 fun WeatherScreenPreview(){
     WeatherCheckTheme {
