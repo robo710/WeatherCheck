90fe1fb98d6f7ca82bc589d32112c3f7745d026f
손찬형 <sonchanhyeong710@gmail.com>
2025-04-28 18:23:35 +0900

    feat: NotificationUseCase 구현

--- app/src/main/java/com/sonchan/weathercheck/domain/usecase/NotificationUseCase.kt
+++ app/src/main/java/com/sonchan/weathercheck/domain/usecase/NotificationUseCase.kt
@@ -1,4 +1,13 @@
 package com.sonchan.weathercheck.domain.usecase
 
-class NotificationUseCase {
+import android.content.Context
+import com.sonchan.weathercheck.domain.repository.NotificationRepository
+import javax.inject.Inject
+
+class NotificationUseCase @Inject constructor(
+    private val repository: NotificationRepository
+){
+    fun showNotification(context: Context){
+        repository.showNotification(context)
+    }
 }
