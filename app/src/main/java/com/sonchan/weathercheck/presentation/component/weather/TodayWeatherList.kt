9a9ab71f29eccc18abbe749025f9ae9fe607f8af
손찬형 <sonchanhyeong710@gmail.com>
2025-04-29 16:18:11 +0900

    feat: TodayWeatherList 수정

--- app/src/main/java/com/sonchan/weathercheck/presentation/component/weather/TodayWeatherList.kt
+++ app/src/main/java/com/sonchan/weathercheck/presentation/component/weather/TodayWeatherList.kt
@@ -1,13 +1,20 @@
 package com.sonchan.weathercheck.presentation.component.weather
 
+import androidx.compose.foundation.layout.Column
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
 import com.sonchan.weathercheck.presentation.component.preview.DarkThemeDevicePreviews
 import com.sonchan.weathercheck.presentation.component.preview.DevicePreviews
 import com.sonchan.weathercheck.ui.theme.WeatherCheckTheme
 
 @Composable
-fun TodayWeatherList(){
-
+fun TodayWeatherList(
+    modifier: Modifier = Modifier
+){
+    Column(
+        modifier
+    ) {
+    }
 }
 
 @DevicePreviews
