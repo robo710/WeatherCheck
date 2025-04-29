4f95726fa302dfc563097ea9fd20f750d63ce00f
손찬형 <sonchanhyeong710@gmail.com>
2025-04-29 17:20:02 +0900

    feat: 간단한 TodayWeatherList 설정

--- app/src/main/java/com/sonchan/weathercheck/presentation/component/weather/TodayWeatherList.kt
+++ app/src/main/java/com/sonchan/weathercheck/presentation/component/weather/TodayWeatherList.kt
@@ -1,19 +1,44 @@
 package com.sonchan.weathercheck.presentation.component.weather
 
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.unit.dp
 import com.sonchan.weathercheck.presentation.component.preview.DarkThemeDevicePreviews
 import com.sonchan.weathercheck.presentation.component.preview.DevicePreviews
 import com.sonchan.weathercheck.ui.theme.WeatherCheckTheme
 
 @Composable
 fun TodayWeatherList(
-    modifier: Modifier = Modifier
+    modifier: Modifier = Modifier,
+    time: String,
+    temp: Int,
+    pop: Int
 ){
     Column(
         modifier
+            .clip(RoundedCornerShape(12.dp))
+            .background(color = MaterialTheme.colorScheme.onBackground)
+            .padding(10.dp)
     ) {
+        Text(
+            text = "${time.substring(0,2)}시",
+            color = MaterialTheme.colorScheme.primary
+        )
+        Text(
+            text = "$temp°C",
+            color = MaterialTheme.colorScheme.primary
+        )
+        Text(
+            text = "$pop%",
+            color = MaterialTheme.colorScheme.primary
+        )
     }
 }
 
@@ -22,6 +47,10 @@ fun TodayWeatherList(
 @Composable
 fun TodayWeatherListPreview(){
     WeatherCheckTheme {
-        TodayWeatherList()
+        TodayWeatherList(
+            time = "0200",
+            temp = 0,
+            pop = 0
+        )
     }
 }
