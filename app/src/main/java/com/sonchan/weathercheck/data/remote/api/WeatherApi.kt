6b04ab4204532e4555615af7c1ae6d44438d298d
손찬형 <sonchanhyeong710@gmail.com>
2025-04-29 15:58:15 +0900

    feat: 가져오는 data량 추가

--- app/src/main/java/com/sonchan/weathercheck/data/remote/api/WeatherApi.kt
+++ app/src/main/java/com/sonchan/weathercheck/data/remote/api/WeatherApi.kt
@@ -9,7 +9,7 @@ interface WeatherApi {
     @GET("getVilageFcst")
     suspend fun getWeatherForecast(
         @Query("serviceKey") serviceKey: String = BuildConfig.WEATHER_API_KEY,
-        @Query("numOfRows") numOfRows: Int = 1000,
+        @Query("numOfRows") numOfRows: Int = 1500,
         @Query("pageNo") pageNo: Int = 1,
         @Query("dataType") dataType: String = "JSON",
         @Query("base_time") baseTime: String,
