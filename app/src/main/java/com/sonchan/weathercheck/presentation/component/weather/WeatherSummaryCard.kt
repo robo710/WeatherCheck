f7d973767fa25afd937dfc1e019640e82ccbdf8e
손찬형 <sonchanhyeong710@gmail.com>
2025-04-30 15:42:12 +0900

    feat: preview setting

--- app/src/main/java/com/sonchan/weathercheck/presentation/component/weather/WeatherSummaryCard.kt
+++ app/src/main/java/com/sonchan/weathercheck/presentation/component/weather/WeatherSummaryCard.kt
@@ -1,29 +1,48 @@
 package com.sonchan.weathercheck.presentation.component.weather
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
-import com.sonchan.weathercheck.domain.model.SkyInfo
+import androidx.compose.ui.Modifier
 import com.sonchan.weathercheck.presentation.component.preview.DarkThemeDevicePreviews
 import com.sonchan.weathercheck.presentation.component.preview.DevicePreviews
 import com.sonchan.weathercheck.ui.theme.WeatherCheckTheme
 
 @Composable
 fun WeatherSummaryCard(
+    modifier: Modifier = Modifier,
     date: String,
     temp: Int,
     pop: Int,
-    sky: SkyInfo,
+    sky: String,
     maxTemp: Int,
     minTemp: Int,
     humidity: Int,
 ){
+    Column(
+        modifier = modifier
+            .fillMaxWidth()
+            .background(MaterialTheme.colorScheme.surface)
+    ) {
 
+    }
 }
 
-//@DevicePreviews
-//@DarkThemeDevicePreviews
-//@Composable
-//fun WeatherSummaryCardPreview(){
-//    WeatherCheckTheme {
-//        WeatherSummaryCard()
-//    }
-//}
+@DevicePreviews
+@DarkThemeDevicePreviews
+@Composable
+fun WeatherSummaryCardPreview(){
+    WeatherCheckTheme {
+        WeatherSummaryCard(
+            date = "20250430",
+            temp = 23,
+            pop = 10,
+            sky = "맑음",
+            maxTemp = 25,
+            minTemp = 18,
+            humidity = 45
+        )
+    }
+}
