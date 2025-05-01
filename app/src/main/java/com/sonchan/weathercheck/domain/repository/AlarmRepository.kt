762acb44614214af5b6167fea3b804189937d8ee
손찬형 <sonchanhyeong710@gmail.com>
2025-05-01 11:54:02 +0900

    feat: AlarmRepository 수정

--- app/src/main/java/com/sonchan/weathercheck/domain/repository/AlarmRepository.kt
+++ app/src/main/java/com/sonchan/weathercheck/domain/repository/AlarmRepository.kt
@@ -1,5 +1,9 @@
 package com.sonchan.weathercheck.domain.repository
 
+import kotlinx.coroutines.flow.Flow
+
 interface AlarmRepository {
     suspend fun saveAlarmTime(hour: Int, minute: Int)
+    fun getAlarmHour(): Flow<Int>
+    fun getAlarmMinute(): Flow<Int>
 }
