92450df0ed35dc326ba8cb835d13da93b00809d0
손찬형 <sonchanhyeong710@gmail.com>
2025-05-01 11:20:50 +0900

    feat: TimePicker 설정

--- app/src/main/java/com/sonchan/weathercheck/presentation/component/setting/TimePicker.kt
+++ app/src/main/java/com/sonchan/weathercheck/presentation/component/setting/TimePicker.kt
@@ -1,8 +1,28 @@
 package com.sonchan.weathercheck.presentation.component.setting
 
+import android.app.TimePickerDialog
+import androidx.compose.material3.Button
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
+import androidx.compose.ui.platform.LocalContext
 
 @Composable
-fun TimePicker(){
+fun TimePickerDialog(
+    onTimeSelected: (hour: Int, minute: Int) -> Unit
+){
+    val context = LocalContext.current
+    val timePickerDialog = remember {
+        TimePickerDialog(
+            context,
+            { _, hour, minute ->
+                onTimeSelected(hour, minute)
+            },
+            8, 0, true
+        )
+    }
 
+    Button(onClick = { timePickerDialog.show() }) {
+        Text("알림 시간 설정")
+    }
 }
